a0857b7b6ffc47ee724bb52accc06d2c39a2b153
tommydebisi <tommydebisi@gmail.com>
2023-03-17 06:59:01 +0100

    :art: endpoints to list income and expenses

--- app/controllers/trackController.js
+++ app/controllers/trackController.js
@@ -5,7 +5,7 @@ const dbClient = require('../utils/db');
 
 class TrackController {
   // Controller for adding income
-  static async income(req, res) {
+  static async addIncome(req, res) {
     const { amount, description } = req.body;
     if (!amount) return res.status(400).json({ error: 'Missing Amount' });
     if (!description) return res.status(400).json({ error: 'Missing Description' });
@@ -19,68 +19,77 @@ class TrackController {
     track.totalIncome += amount;
     track.currentBalance = track.totalIncome - track.totalExpenses;
 
+    // date: { $dateToString: { format: "%Y-%m-%d", date: "$$lastIncome.createdAt" },
+    // time: { $dateToString: { format: "%H:%M:%S", date: "$$lastIncome.createdAt" }
 
-    // Use aggregation framework to extract date and time values
-    const incomeWithDate = await Track.aggregate([
-      { $match: { _id: track._id } },
-      { $project: {
-          currentBalance: 1,
-          income: {
-            $let: {
-              vars: {
-                lastIncome: { $arrayElemAt: [ "$income", 1 ] }
-              },
-              in: {
-                type: "$$lastIncome.type",
-                amount: "$$lastIncome.amount",
-                description: "$$lastIncome.description",
-                currentBalance: "$$lastIncome.currentBalance",
-                _id: "$$lastIncome._id",
-                date: { $dateToString: { format: "%Y-%m-%d", date: "$$lastIncome.createdAt" } },
-                time: { $dateToString: { format: "%H:%M:%S", date: "$$lastIncome.createdAt" } }
-              }
-            }
-          }
-        }
-      }
-    ]);
-  
-    const modifiedIncome = incomeWithDate[0].income;
-  
     // save track
     track.save();
-  
-    return res.status(201).json({
-      currentBalance: track.currentBalance,
-      income: modifiedIncome
-    });
-    
-  }
-  
 
-   // Controller for adding Expenses
-   static async expense(req, res) {
+    return res.status(201).json({ message: 'success' });
+  }
+
+  // Controller for adding Expenses
+  static async addExpense(req, res) {
     const { amount, description } = req.body;
     if (!amount) return res.status(400).json({ error: 'Missing Amount' });
     if (!description) return res.status(400).json({ error: 'Missing Description' });
 
     // get the current track by user id
     const track = await dbClient.getSchemaOne(Track, { userId: new Types.ObjectId(req.userId) });
-    const expensesCB = track.currentBalance + amount;
+    const expensesCB = track.currentBalance - amount;
     track.expense.push({ amount, description, currentBalance: expensesCB });
 
     // add expenses to total income
     track.totalExpenses += amount;
     track.currentBalance = track.totalIncome - track.totalExpenses;
-
     track.save();
 
-    return res.status(201).json({
-      currentBalance: track.currentBalance, income: track.income.slice(-1)[0]
-    });
+    return res.status(201).json({ message: 'success' });
   }
 
-  
+  // controller for adding new current balance
+  static async addCurrentBal(req, res) {
+    const { currentBalance } = req.body;
+    if (!currentBalance) return res.status(400).json({ error: 'Missing CurrentBalance' });
+
+    // get the current track by user id
+    const track = await dbClient.getSchemaOne(Track, { userId: new Types.ObjectId(req.userId) });
+
+    track.currentBalance = currentBalance;
+    track.totalIncome = currentBalance;
+    track.save();
+
+    return res.status(201).json({ message: 'success' });
+  }
+
+  // Controller for getting paginated list of income and expenses
+  static async listIncomeExpense(req, res) {
+    // get the page or limit to display from query string
+    let limit, page;
+    limit = req.query.limit || 5;
+    page = req.query.page || 0;
+
+    // convert to numbers if present
+    limit = Number(limit);
+    page = Number(page);
+
+    // get the current track by user id
+    const track = await dbClient.getSchemaOne(Track, { userId: new Types.ObjectId(req.userId) });
+
+    try {
+      // get income and expense list
+      const incomeList = await dbClient.getIncomeList(Track, track._id, page, limit);
+      const expenseList = await dbClient.getExpenseList(Track, track._id, page, limit);
+
+      return res.status(200).json({
+        currentBalance: track.currentBalance,
+        incomes: incomeList,
+        expenses: expenseList,
+      });
+    } catch (e) {
+      return res.status(400).json({ error: e.message });
+    }
+  }
 }
 
 module.exports = TrackController;
--- app/routes/trackRoute.js
+++ app/routes/trackRoute.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const TrackController = require('../controllers/trackController');
 const authToken = require('../middlewares/authToken');
 
-
-router.route('/income').post(authToken, TrackController.income);
-router.route('/expenses').post(authToken, TrackController.expense);
-
+router.route('/income').post(authToken, TrackController.addIncome);
+router.route('/expense').post(authToken, TrackController.addExpense);
+router.route('/balance').post(authToken, TrackController.addCurrentBal);
+router.route('/trackIEs').get(authToken, TrackController.listIncomeExpense);
 
 module.exports = router;
