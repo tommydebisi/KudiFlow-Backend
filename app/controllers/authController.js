fdf2d38af28c6d2b78024e5d4aa12e6de90c5f0e
tommydebisi <tommydebisi@gmail.com>
2023-04-04 16:47:23 +0100

    :seedling: made login, reset-token, forgot GET req

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,30 +1,33 @@
-const jwt = require('jsonwebtoken');
-const { compare } = require('bcrypt');
-const { Types } = require('mongoose');
-const { randomBytes } = require('crypto');
-const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
-const { User } = require('../models/User');
-const Track = require('../models/Track');
-const { hashPassword, generateAccessToken, sendEmail } = require('../utils/helper');
-const { createUserSchema, loginUserSchema } = require('../validators/Validate');
-
-
+const jwt = require("jsonwebtoken");
+const { compare } = require("bcrypt");
+const { Types } = require("mongoose");
+const { randomBytes } = require("crypto");
+const dbClient = require("../utils/db");
+const redisClient = require("../utils/redis");
+const { User } = require("../models/User");
+const Track = require("../models/Track");
+const {
+  hashPassword,
+  generateAccessToken,
+  sendEmail,
+} = require("../utils/helper");
+const { createUserSchema, loginUserSchema } = require("../validators/Validate");
 
 class AuthController {
   static async signUp(req, res) {
     const { username, email, password } = req.body;
 
-    if (!username) return res.status(400).json({ error: 'Missing username' });
-    if (!email) return res.status(400).json({ error: 'Missing email' });
-    if (!password) return res.status(400).json({ error: 'Missing password' });
+    if (!username) return res.status(400).json({ error: "Missing username" });
+    if (!email) return res.status(400).json({ error: "Missing email" });
+    if (!password) return res.status(400).json({ error: "Missing password" });
 
     // validate password meets criteria
     await createUserSchema.validateAsync({ email, password });
 
     // Check if user already exists
     const existingUser = await dbClient.getSchemaOne(User, { email });
-    if (existingUser) return res.status(400).json({ error: 'User already exists' });
+    if (existingUser)
+      return res.status(400).json({ error: "User already exists" });
 
     const hashed_password = await hashPassword(password);
     try {
@@ -33,63 +36,69 @@ class AuthController {
       // create new track for user
       const track = await Track.create({ userId: user._id });
 
-      return res.status(201).json({ message: 'success' });
+      return res.status(201).json({ message: "success" });
     } catch (error) {
       return res.status(400).json({ error: error.message });
     }
-  };
+  }
 
   static async signIn(req, res) {
     const { email, password } = req.body;
 
     // check if email and password is present
-    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!email) return res.status(400).json({ error: "Missing email" });
 
-   // validate email using Joi schema
+    // validate email using Joi schema
     /**const { error } = await loginUserSchema.validateAsync({ email });
     if (error) {
       return res.status(400).json({ error: 'Invalid email format' });
     }
 **/
-    if (!password) return res.status(400).json({ error: 'Missing password' });
+    if (!password) return res.status(400).json({ error: "Missing password" });
 
     const user = await dbClient.getSchemaOne(User, { email });
     if (!user || !(await compare(password, user.hashed_password))) {
-      return res.status(400).json({ error: 'Invalid email or password' });
+      return res.status(400).json({ error: "Invalid email or password" });
     }
     //signing token with user id
     const accessToken = generateAccessToken({ id: user._id.toString() });
-    const refreshToken = jwt.sign({ id: user._id.toString() }, process.env.API_SECRET_REFRESH);
+    const refreshToken = jwt.sign(
+      { id: user._id.toString() },
+      process.env.API_SECRET_REFRESH
+    );
 
     // save refresh token in redis
     const result = await redisClient.set(`auth_${accessToken}`, refreshToken);
 
     //responding to client request access and request tokens.
-    res.status(202)
-      .json({ accessToken });
+    res.status(202).json({ accessToken });
   }
 
   static async getNewToken(req, res) {
     // get expired access token from request body
-    const { token } = req.body
+    const { token } = req.body;
 
-    if (!token) return res.status(400).json({ error: 'Missing Token' });
+    if (!token) return res.status(400).json({ error: "Missing Token" });
     const refreshToken = await redisClient.get(`auth_${token}`);
 
     // check if token is available
-    if (!refreshToken) return res.status(403).json({ error: 'Forbidden' });
+    if (!refreshToken) return res.status(403).json({ error: "Forbidden" });
 
     // verify refresh token
-    jwt.verify(refreshToken, process.env.API_SECRET_REFRESH, async (err, field) => {
-      if (err) return res.status(403).json({ error: 'Unable to verify token' })
+    jwt.verify(
+      refreshToken,
+      process.env.API_SECRET_REFRESH,
+      async (err, field) => {
+        if (err)
+          return res.status(403).json({ error: "Unable to verify token" });
 
-      const accessToken = generateAccessToken({ id: field.id })
-      // remove previous accessToken
-      await redisClient.del(`auth_${token}`);
-      await redisClient.set(`auth_ ${accessToken}`, refreshToken);
-      return res.status(200).json({ accessToken });
-    });
-
+        const accessToken = generateAccessToken({ id: field.id });
+        // remove previous accessToken
+        await redisClient.del(`auth_${token}`);
+        await redisClient.set(`auth_ ${accessToken}`, refreshToken);
+        return res.status(200).json({ accessToken });
+      }
+    );
   }
   static async logout(req, res) {
     await redisClient.del(`auth_${req.token}`);
@@ -103,7 +112,7 @@ class AuthController {
     const user = await dbClient.getSchemaOne(User, { email });
 
     if (!user) return res.status(404).json({ error: "Not Found" });
-    const resetToken = require('crypto').randomBytes(30).toString('hex');
+    const resetToken = require("crypto").randomBytes(30).toString("hex");
 
     try {
       // store reset password with expiry for 4min
@@ -115,19 +124,21 @@ class AuthController {
     // send email with reset token
     sendEmail(resetToken, email);
 
-    return res.status(202).json({ message: 'success' });
+    return res.status(202).json({ message: "success" });
   }
 
   static async postReset(req, res) {
     // get and update user with id, change userId in str to objectId
-    const user = await dbClient.getSchemaOne(User, { _id: new Types.ObjectId(req.userId) });
+    const user = await dbClient.getSchemaOne(User, {
+      _id: new Types.ObjectId(req.userId),
+    });
 
-    if (!user) return res.send('Error getting user');
+    if (!user) return res.send("Error getting user");
 
     user.hashed_password = await hashPassword(req.newPass);
     user.save();
 
-    return res.status(200).json({ message: 'success' })
+    return res.status(200).json({ message: "success" });
   }
 }
 
--- app/routes/v1/authRoute.js
+++ app/routes/v1/authRoute.js
@@ -6,10 +6,10 @@ const router = express.Router();
 
 
 router.route('/register').post(AuthController.signUp);
-router.route('/login').get(AuthController.signIn);
+router.route('/login').post(AuthController.signIn);
 router.route('/logout').get(authToken, AuthController.logout);
-router.route('/token-reset').get(AuthController.getNewToken);
-router.route('/forgot').get(AuthController.forgot);
+router.route('/token-reset').post(AuthController.getNewToken);
+router.route('/forgot').post(AuthController.forgot);
 router.route('/password-reset').post(confirmPass, AuthController.postReset);
 
 module.exports = router;
