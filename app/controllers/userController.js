d9d719a11d4fe8d8c702605c224dacbef470036f
tommydebisi <tommydebisi@gmail.com>
2023-04-24 09:58:28 +0100

    :seedling: validated the new password

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,7 +1,8 @@
 const { Types } = require("mongoose");
 const { User } = require("../models/User");
 const dbClient = require("../utils/db");
-const { hashPassword } = require('../utils/helper')
+const { hashPassword } = require('../utils/helper');
+const { createUserSchema } = require('../validators/Validate');
 
 class UserController {
   static async userPassReset(req, res) {
@@ -11,6 +12,14 @@ class UserController {
     const user = await dbClient.getSchemaOne(User, {
       _id: new Types.ObjectId(req.userId),
     });
+    const userEmail = user.email;
+
+    // validate password meets criteria
+    try {
+      await createUserSchema.validateAsync({ userEmail, newPassword });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
 
     user.hashed_password = await hashPassword(newPassword);
     user.save();
